0629fe6c6061dc194a33126d3b77df437653ace5
Fadi Gorges <fadi.gorges.03@gmail.com>
2025-08-05 20:08:50 +1000

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  output: 'standalone'
+};
+
+export default nextConfig;
